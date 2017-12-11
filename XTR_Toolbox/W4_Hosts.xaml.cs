c8f316370528e9718c0b5102df5b9e793638a378
Zeeex <16362320+Zeeex@users.noreply.github.com>
2017-12-11 19:54:35 +0100

    Cleaned up project and reworked hosts file manager

---
+++ XTR_Toolbox/W4_Hosts.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace XTR_Toolbox
+{
+    public partial class Window4
+    {
+        private readonly string _hostsFilePath =
+            Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.System), "drivers/etc/hosts");
+
+        public Window4()
+        {
+            InitializeComponent();
+            HostsLoad();
+        }
+
+        private void BtnHostsManage_Click(object sender, RoutedEventArgs e)
+        {
+            if (Equals(sender, BtnHostsReset))
+            {
+                string[] lines = TbHostsFile.Text.Split('\n');
+                List<string> comments = lines.Where(line => line.TrimStart(' ').StartsWith("#")).ToList();
+                TbHostsFile.Text = string.Join("\n", comments);
+                BtnHostsSave.IsEnabled = false;
+            }
+            try
+            {
+                using (StreamWriter file = File.CreateText(_hostsFilePath))
+                {
+                    file.Write(TbHostsFile.Text);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            TbHostsFile.Foreground = Brushes.White;
+            TbHostsFile.Background = Brushes.Black;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            TbHostsFile.Foreground = Brushes.Black;
+            TbHostsFile.Background = Brushes.White;
+        }
+
+        private void HostsLoad()
+        {
+            try
+            {
+                if (!File.Exists(_hostsFilePath)) throw new Exception();
+                TbHostsFile.TextChanged -= TbHostsFile_TextChanged;
+                TbHostsFile.Text = File.ReadAllText(_hostsFilePath);
+                TbHostsFile.TextChanged += TbHostsFile_TextChanged;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Opacity = 0;
+                Loaded += (s, e) => Close();
+            }
+        }
+
+        private void OnCloseExecuted(object sender, ExecutedRoutedEventArgs e) => Close();
+
+        private void TbHostsFile_TextChanged(object sender, EventArgs e) => BtnHostsSave.IsEnabled = true;
+    }
+}
